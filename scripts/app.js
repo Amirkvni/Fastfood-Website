04ad6ad6457999b700ad19d0770b1aa4812b7a0e
amirhosein keivani <amirhosein8292@gmail.com>
2024-03-09 19:54:11 +0330

    coding navbar section

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 const menu_link = document.querySelector(".page-language");
 const menu_list = document.querySelector(".menu__list");
 const menu_link_icon = document.querySelector(".page-language__icon");
+const nav_icon = document.querySelector(".nav-icon");
+const navbar_collapse = document.querySelector(".navbar-collapse");
 menu_link.addEventListener("click", function () {
   menu_list.classList.toggle("menu__list--show");
   if (menu_link_icon.classList.contains("fa-angle-down")) {
@@ -9,5 +11,16 @@ menu_link.addEventListener("click", function () {
   } else {
     menu_link_icon.classList.remove("fa-angle-up");
     menu_link_icon.classList.add("fa-angle-down");
+  }
+});
+
+nav_icon.addEventListener("click", function () {
+  navbar_collapse.classList.toggle("navbar-collapse--open");
+  if (nav_icon.classList.contains("fa-bars")) {
+    nav_icon.classList.remove("fa-bars");
+    nav_icon.classList.add("fa-xmark");
+  } else if (nav_icon.classList.contains("fa-xmark")) {
+    nav_icon.classList.remove("fa-xmark");
+    nav_icon.classList.add("fa-bars");
   }
 });
