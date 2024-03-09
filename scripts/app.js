c6e6088e01e9f599d912381eaa8c708e1024a731
amirhosein keivani <amirhosein8292@gmail.com>
2024-03-09 22:34:31 +0330

    complete navbar section

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ const menu_list = document.querySelector(".menu__list");
 const menu_link_icon = document.querySelector(".page-language__icon");
 const nav_icon = document.querySelector(".nav-icon");
 const navbar_collapse = document.querySelector(".navbar-collapse");
+const blogIcon = document.querySelector(".blog-icon");
 menu_link.addEventListener("click", function () {
   menu_list.classList.toggle("menu__list--show");
   if (menu_link_icon.classList.contains("fa-angle-down")) {
@@ -22,5 +23,29 @@ nav_icon.addEventListener("click", function () {
   } else if (nav_icon.classList.contains("fa-xmark")) {
     nav_icon.classList.remove("fa-xmark");
     nav_icon.classList.add("fa-bars");
+  }
+});
+blogIcon.addEventListener("mouseenter", function () {
+  if (blogIcon.classList.contains("fa-angle-down")) {
+    blogIcon.classList.remove("fa-angle-down");
+    blogIcon.classList.add("fa-angle-up");
+  }
+});
+blogIcon.addEventListener("mouseleave", function () {
+  if (blogIcon.classList.contains("fa-angle-up")) {
+    blogIcon.classList.remove("fa-angle-up");
+    blogIcon.classList.add("fa-angle-down");
+  }
+});
+blogIcon.addEventListener("touchstart", function () {
+  if (blogIcon.classList.contains("fa-angle-down")) {
+    blogIcon.classList.remove("fa-angle-down");
+    blogIcon.classList.add("fa-angle-up");
+  }
+});
+blogIcon.addEventListener("touchend", function () {
+  if (blogIcon.classList.contains("fa-angle-up")) {
+    blogIcon.classList.remove("fa-angle-up");
+    blogIcon.classList.add("fa-angle-down");
   }
 });
