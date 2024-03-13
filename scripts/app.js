682f69792e25e0699a0269e355d36260d7ed667c
amirhosein keivani <amirhosein8292@gmail.com>
2024-03-14 01:49:17 +0330

    refactor submenu js codes

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,8 @@ const menu_link_icon = document.querySelector(".page-language__icon");
 const nav_icon = document.querySelector(".nav-icon");
 const navbar_collapse = document.querySelector(".navbar-collapse");
 const blogIcon = document.querySelector(".blog-icon");
+let sub_menu = document.querySelector(".sub_menu");
+let blog_nav__item = document.querySelector(".blog-nav__item");
 menu_link.addEventListener("click", function () {
   menu_list.classList.toggle("menu__list--show");
   if (menu_link_icon.classList.contains("fa-angle-down")) {
@@ -37,15 +39,9 @@ blogIcon.addEventListener("mouseleave", function () {
     blogIcon.classList.add("fa-angle-down");
   }
 });
-blogIcon.addEventListener("touchstart", function () {
-  if (blogIcon.classList.contains("fa-angle-down")) {
-    blogIcon.classList.remove("fa-angle-down");
-    blogIcon.classList.add("fa-angle-up");
-  }
+blog_nav__item.addEventListener("mouseenter", function () {
+  sub_menu.classList.add("sub_menu--active");
 });
-blogIcon.addEventListener("touchend", function () {
-  if (blogIcon.classList.contains("fa-angle-up")) {
-    blogIcon.classList.remove("fa-angle-up");
-    blogIcon.classList.add("fa-angle-down");
-  }
+sub_menu.addEventListener("mouseleave", function () {
+  sub_menu.classList.remove("sub_menu--active");
 });
